441e0ffe8fed3e99b3cfaefcf530c0793ff2cf4d
uenop <dummy@mail.com>
2012-02-20 02:10:48 +0900

    key-chord.el

---
+++ conf/init-chord.el
@@ -0,0 +1,24 @@
+;;; init-chord.el --- 
+
+(require 'key-chord)
+(key-chord-mode t)
+
+(setq key-chord-two-keys-delay 0.04)
+
+(defun test-keychord()
+  (interactive)
+  (view-mode)
+  (message "hoge"))
+  
+(key-chord-define-global "jk" '(lambda()
+                                 (interactive)
+                                 (view-mode)
+                                 (message (format "%s" view-mode))))
+;(key-chord-define-global "jk" 'test-keychord)
+
+
+(key-chord-define-global "hj" 'goto-line)
+(key-chord-define-global "ja" 'jaunte)
+(key-chord-define-global ",."     "<>\C-b")
+;; (key-chord-define-global [?h ?j]  'undo)  ; the same
+(key-chord-define-global "cv"     'reindent-then-newline-and-indent)
